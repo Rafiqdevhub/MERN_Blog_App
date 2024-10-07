79499efdcd1ca3daa06587faff42114f63dc61b0
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-07 14:48:33 +0500

    Authentication is added to the user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,34 +3,67 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { errorHandler } = require("../utils/errors");
 
-const registerUser = async (req, res) => {
-  const { username, email, password } = req.body;
-
-  if (
-    !username ||
-    !email ||
-    !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
-  ) {
-    next(errorHandler(400, "All fields are required"));
-  }
-
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-
-  const newUser = new User({
-    username,
-    email,
-    password: hashedPassword,
-  });
-
+const registerUser = async (req, res, next) => {
   try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return next(errorHandler(400, "All fields are required"));
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
     await newUser.save();
     res.json("Signup successful");
   } catch (error) {
-    next(error);
+    console.error(error);
+    next(errorHandler(500, "Internal server error"));
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      })
+      .json(rest);
+  } catch (error) {
+    console.error(error);
+    next(errorHandler(500, "Internal server error"));
+  }
+};
+
+module.exports = { registerUser, loginUser };
--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ app.get("/*", (req, res) => {
   });
 });
 
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    message,
+  });
+});
+
 app.listen(port, () => {
   connectionDb();
   console.log(`Server is running on http://localhost:${port}`);
