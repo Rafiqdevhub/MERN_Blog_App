6d7a9b388ee9eee796b8d7aa67fb815fd2f738f6
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-08 20:34:14 +0500

    google route is added

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  google,
+} = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/google", google);
 module.exports = router;
