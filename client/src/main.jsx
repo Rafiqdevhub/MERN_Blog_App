fe7fa116634ac7bde9e3df784076327183d8f1c6
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-08 20:39:28 +0500

    Dark mood is added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,14 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.js";
-
+import ThemeProvider from "./components/ThemeProvider.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   </StrictMode>
