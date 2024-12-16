e7b93f48ba47f238fd22bb5948cbfc20f02a46eb
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-12-16 21:15:02 +0500

    Add logging for token verification process in verifyUser utility

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -4,12 +4,15 @@ import { errorHandler } from "./errors.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
+    console.log("No token found in cookies");
     return next(errorHandler(401, "Unauthorized"));
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
+      console.log("JWT verification failed:", err.message);
       return next(errorHandler(401, "Unauthorized"));
     }
+    console.log("User authenticated:", user);
     req.user = user;
     next();
   });
