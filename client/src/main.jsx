b0be6ef81b9587b99142730dc7428ba25b99712e
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-12-20 20:26:44 +0500

    Reorder Provider and PersistGate components for improved readability

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,12 +8,12 @@ import { persistor, store } from "./redux/store.js";
 import ThemeProvider from "./components/ThemeProvider.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
         <ThemeProvider>
           <App />
         </ThemeProvider>
-      </PersistGate>
-    </Provider>
+      </Provider>
+    </PersistGate>
   </StrictMode>
 );
