49458c2bbb2f4f227313e8a409dd5e74c8178d14
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-12-26 21:12:52 +0500

    Remove unnecessary blank line in App component for cleaner code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -15,6 +14,7 @@ import PostPage from "./pages/PostPage";
 import Projects from "./pages/Projects";
 import ScrollToTop from "./components/ScrollToTop";
 import About from "./pages/About";
+
 const App = () => {
   return (
     <BrowserRouter>
