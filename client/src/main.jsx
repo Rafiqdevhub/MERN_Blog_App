480d2b6e24ae6ada65487829c1962fab8417276d
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-12-27 17:47:19 +0500

    Reorder imports in main.jsx for improved readability and organization

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
+import "./index.css";
+import App from "./App.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.js";
 import ThemeProvider from "./components/ThemeProvider.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <PersistGate persistor={persistor}>
